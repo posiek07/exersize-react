daf65f8af2415e02098386d039b4004487f5a7d0
posiek07 <daniel.smolinski@hotmail.com>
2020-05-23 18:57:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>    Learn React      </p>
+        
+        <Person />
+        <Person />
+        <Person />
+        <Person />
       </header>
+      
     </div>
-  );
+
+    //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'hi, I\'m a React App'))
+    )  
+  }
 }
 
 export default App;
